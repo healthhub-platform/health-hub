6e96b7d0147cb6441dae558f4951b992ad8588be
ranishah0402 <ranishah@bu.edu>
2023-04-29 15:07:24 -0400

    added github pages and description

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Health Hub</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
+        HealthHub is an all women student-led initiative aiming to establish a novel online platform that serves the pre-health community. Through this platform, students and professionals can seek and post about professional development opportunities. The main objectives of this initiative are: <br/>
+        Bridge the gap between students and professionals <br/>
+        Promote enriching opportunities for post-graduate studies to students <br/>
+        Create easier access to research and clinical opportunities. <br/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
