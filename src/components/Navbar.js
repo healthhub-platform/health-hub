78ff8a56246d42afb704677f584324ae9e36cfd3
Nupur <ndivekar@bu.edu>
2023-08-24 14:06:12 +0530

    Updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
+
 const Navbar= () => {
     return (
         <div className="navigation">
-            <li>
-                <NavLink to="/">Homepage</NavLink>
-            </li>
-            <li>
-                <NavLink to="/Profiles">Profiles</NavLink>
-            </li>
+            <ul>
+                <li>
+                    <NavLink to="/">Homepage</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/Profiles">Profiles</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/AcademicCourses">Academic Courses</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/ResearchExperience">Research Experience</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/ClinicalExperience">Clinical Experience</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/StudentOrganisations">Student Organisations</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/Athletics">Athletics</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/SpecialInterest">Special Interest</NavLink>
+                </li>
+            </ul>
         </div>
     );
 }
