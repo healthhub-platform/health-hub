82b9c9019a65a6edea066be3b0792f70c7c950f6
ranishah0402 <ranishah@bu.edu>
2023-08-31 19:01:30 -0500

    added parts of profile subpages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage/Homepage';
 import ProfileMain from './pages/Profile/ProfileMain';
+import AcademicCourses from './pages/Profile/AcademicCourses';
+import ResearchExperience from './pages/Profile/ResearchExperience';
+import ClinicalExperience from './pages/Profile/ClinicalExperience';
+import StudentOrganisations from './pages/Profile/StudentOrgs';
+import Athletics from './pages/Profile/Athletics';
+import SpecialInterest from './pages/Profile/SpecialInterest';
+import StudentOrganisations from './pages/Profile/StudentOrgs';
 
 function App() {
   return (
@@ -14,6 +21,15 @@ function App() {
         <Routes>
           <Route path='/' element={<Homepage/>}/> 
           <Route path='/Profiles' element={<ProfileMain/>}/>
+          <Route path='/AcademicCourses' element={<AcademicCourses/>}/>
+          <Route path='/ResearchExperience' element={<ResearchExperience/>}/>
+          <Route path='/ClinicalExperience' element={<ClinicalExperience/>}/>
+          <Route path='/StudentOrganisations' element={<StudentOrganisations/>}/>
+          <Route path='/Athletics' element={<Athletics/>}/>
+          <Route path='/SpecialInterest' element={<SpecialInterest/>}/>
+          <Route path='/StudentOrgs' element={<StudentOrganisations/>}/>
+
+          
         </Routes>
       </Router>
       </div>
---
+++ src/pages/Profile/ResearchExperience.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ProfileMain.css';
+const ResearchExperience = () => {
+    return (
+        <div className='container'>
+            <h3>Research Experience</h3>
+
+            <ul className = "sidebar">
+                <li> <span> Jane Doe </span></li>
+                <li><span><i className="fa fa-title"></i> </span><span>Title</span> </li>
+                <li><span><i className="fa fa-school"></i> </span><span>Education: <br /> High School <br /> College <br /> Certification Programs </span> </li>
+                <li><span><i className="fa fa-qualifications"></i> </span><span>Qualifications: <br /> Certificate 1 <br /> Certificate 2 <br /> Certificate 3 </span> </li>
+                <li><span><i className="fa fa-SeekingPositions"></i> </span><span>Seeking Opportunities In: <br/> list fields </span> </li>
+                <li><span><i className="fa fa-interests"></i> </span><span>Interests: </span> </li>
+                <li><span><i className="fa fa-location"></i> </span><span>Willing to Relocate: Y/N </span> </li>
+            </ul>
+
+            <div className="content"> 
+            
+            </div>
+        </div>
+    );
+}
+export default ResearchExperience;
